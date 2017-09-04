cb470be96365202234043979ac8e3a95907e2a69
mac718 <mac718@gmail.com>
2017-09-04 11:56:25 -0400

    Added valid_move? method

--- toh.rb
+++ toh.rb
@@ -6,10 +6,17 @@ end
 
 def make_move(board, move)
   board[move[1]].unshift(board[move[0]].shift)
+  display_board(board)
 end
 
 def display_board(board)
   p board
+end
+
+def valid_move?(tower_height, move, board)
+  move.length == 3 && (1..tower_height).include?(move[0].to_i) && 
+  move[1] == ',' && (1..tower_height).include?(move[2].to_i) &&
+  (board[move[2].to_i - 1].empty? || board[move[0].to_i - 1][0] < board[move[2].to_i - 1][0])
 end
 
 def win?(board, tower_height)
@@ -20,24 +27,18 @@ puts "Specify tower height"
 tower_height = gets.chomp.to_i 
 
 board = initialize_board(tower_height)
+display_board(board)
 
 loop do 
   puts "Enter move:"
   move = gets.chomp
-  while move.length != 3 || !(1..tower_height).include?(move[0].to_i)  || move[1] != ',' || !(1..tower_height).include?(move[2].to_i)
+  while !valid_move?(tower_height, move, board)
     puts "Invalid move, try again:"
     move = gets.chomp
   end
   move = [move[0].to_i - 1, move[2].to_i - 1]
-  if board[move[1]].empty?
-    make_move(board, move)
-    display_board(board)
-  elsif board[move[0]][0] > board[move[1]][0]
-    puts "invalid move"
-    break 
-  else
-    make_move(board, move)
-    display_board(board)
-  end
+  make_move(board, move)
   break if win?(board, tower_height)
 end
+
+puts "You win!"
