4e1892d56c8d5c28682bd12d79268de86878d481
mac718 <mac718@gmail.com>
2017-09-04 19:35:47 -0400

    Added an ugly display_board method

--- toh.rb
+++ toh.rb
@@ -4,13 +4,40 @@ def initialize_board(tower_height)
   board = [(1..tower_height).to_a, [], []]
 end
 
-def make_move(board, move)
+def make_move(board, move, tower_height)
   board[move[1]].unshift(board[move[0]].shift)
-  display_board(board)
+  display_board(board, tower_height)
 end
 
-def display_board(board)
-  p board
+def display_board(board, tower_height)
+  peg1 = [[]]
+
+  peg2 =  [[]]
+
+  peg3 = [[]]
+
+
+  board[0].each do |disc|
+      peg1[0] << 'o' * disc 
+  end 
+
+  (tower_height-board[0].length).times { peg1[0].unshift(' ' * tower_height) }
+
+  board[1].each do |disc|
+      peg2[0] << 'o' * disc 
+  end 
+
+  (tower_height-board[1].length).times { peg2[0].unshift(' ' * tower_height) }
+
+
+  board[2].each do |disc|
+      peg3[0] << 'o' * disc 
+  end 
+
+  (tower_height-board[2].length).times { peg3[0].unshift(' ' * tower_height) }
+
+  tower_height.times { |i| puts peg1[0][i] + ' ' + peg2[0][i] + ' ' + peg3[0][i] }
+
 end
 
 def valid_move?(tower_height, move, board)
@@ -27,7 +54,7 @@ puts "Specify tower height"
 tower_height = gets.chomp.to_i 
 
 board = initialize_board(tower_height)
-display_board(board)
+display_board(board, tower_height)
 
 loop do 
   puts "Enter move:"
@@ -37,7 +64,7 @@ loop do
     move = gets.chomp
   end
   move = [move[0].to_i - 1, move[2].to_i - 1]
-  make_move(board, move)
+  make_move(board, move, tower_height)
   break if win?(board, tower_height)
 end
 
